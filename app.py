4b8d27d8265e11fcdea775c8c98d075c8945c2ff
“[udit <[tudrishu@gmail.com]>
2024-08-15 00:13:29 +0530

    pull

--- app.py
+++ app.py
@@ -5,51 +5,51 @@ with open('model.pkl', 'rb') as file:
     model = pickle.load(file)
     
 age = st.number_input(
-    "Enter your age", value=None , placeholder= "age" , key="age",
+    "Enter your age", value=0 , placeholder= "age" , key="age",
 )
 
 gender = st.selectbox(
     "gender",
     ("male", "female"),
-    index=None , placeholder= "gender" , 
+    index=0 , placeholder= "gender" , 
 )
 gender = 0 if gender == "male" else 1
 
 
 blood = st.number_input(
-    "Blood pressure at rest in mm hg", value=None , placeholder="Enter blood pressure" ,key="blood",
+    "Blood pressure at rest in mm hg", value=0 , placeholder="Enter blood pressure" ,key="blood",
 )
 
 chol = st.number_input(
-    "Serum cholesterol in mg/dl", value=None , placeholder="Enter cholestrol" ,key="chol",
+    "Serum cholesterol in mg/dl", value=0 , placeholder="Enter cholestrol" ,key="chol",
 )
 
 ofbs = ["Above 120 mg/dl", "Below 120 mg/dl"]
 fbs = st.selectbox(
     "Fasting blood sugar level",
     ofbs,
-    index=None,
+    index=0,
 )
 pfbs = 1 if fbs == 'Above 120 mg/dl' else 0
 
 Thalach = st.number_input(
-   "Enter Thalach value" , value=None , key="Thalach" , placeholder="Maximum heart rate achieved during a stress test" ,
+   "Enter Thalach value" , value=0 , key="Thalach" , placeholder="Maximum heart rate achieved during a stress test" ,
 )
 exang = st.selectbox(
     "Exercise-induced angina",
     ("Yes", "No"),
-    index=None , placeholder="chest pain during exercise" , key="exang" ,
+    index=0 , placeholder="chest pain during exercise" , key="exang" ,
 )
 exang = 1 if exang == 'Yes' else 0
 
 
 oldpeak = st.number_input(
-   "Enter oldpeak value" , value=None , key="oldpeak" , placeholder="Enter old peak value in mm" ,
+   "Enter oldpeak value" , value=0 , key="oldpeak" , placeholder="Enter old peak value in mm" ,
 )
 slope = st.selectbox(
     "Slope of the peak exercise ST segment:",
     ("Upslopping", "Flat", "Downsloping"),
-    index=None , placeholder="Enter slope value" , key="slope" ,
+    index=0 , placeholder="Enter slope value" , key="slope" ,
 )
 if slope == "Upslopping":
     slope = 0
@@ -61,12 +61,12 @@ else:
 ca = st.selectbox(
     "Number of major vessels (0-4) colored by fluoroscopy:",
     ("0", "1", "2" , "3" , "4" ),
-    index=None , placeholder="Enter slope value" , key="ca" ,
+    index=0 , placeholder="Enter slope value" , key="ca" ,
 )
 cp1 = st.selectbox(
     "Chest pain:",
     ("Typical angina", "Atypical angina", "Non-anginal pain" , "Asymptomatic" ),
-    index=None , placeholder="Chest Pain Type :" , key="cp" ,
+    index=0 , placeholder="Chest Pain Type :" , key="cp" ,
 )
 def angina_classification(cp):
     if cp == "Typical angina":
@@ -84,7 +84,7 @@ chest = angina_classification(cp1)
 ecg1 = st.selectbox(
     "Resting electrocardiographic results :",
     ("Normal", "Having ST-T wave abnormality", "Showing probable or definite left ventricular hypertrophy"),
-    index=None , placeholder="Select ecg results" , key="ecg" ,
+    index=0 , placeholder="Select ecg results" , key="ecg" ,
 )
 def elec(ecg):
     if ecg == "Normal":
@@ -100,7 +100,7 @@ electro = elec(ecg1)
 thal_stress = st.selectbox(
     "Thalium stress test result:",
     ("Normal", "Fixed defect", "Reversible defect" , "Not described" ),
-    index=None , placeholder="Chest Pain Type :" , key="thal" ,
+    index=0 , placeholder="Chest Pain Type :" , key="thal" ,
 )
 
 def thala(thal):
